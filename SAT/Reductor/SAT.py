2df6e47489642e3656448d371b952f4d714d3f9d
vvbv <jeison.cardona@mgwebstudio.com>
2020-09-19 20:35:04 -0500

    Fix mistake

--- SAT/Reductor/SAT.py
+++ SAT/Reductor/SAT.py
@@ -1,39 +1,46 @@
 class SATvar:
 
-    def __init__(self, value: str):
-        # self.name = name
-        # self.value = value
-        
-    # @property
-    # def name(self) -> str:
-    #     return self._name
-    #
-    # @name.setter
-    # def name(self, value:str):
-    #     self._name = value
-    #
-    # @property
-    # def value(self) -> bool:
-    #     return self._value
-    #
-    # @value.setter
-    # def value(self, value: bool):
-    #     self._value = value
-        pass
+    def __init__(self, var_str: str):
+        self.value = False if var_str[0] == '-' else True
+        self.name = var_str if self.value else var_str.replace('-','')
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, value:str):
+        self._name = value
+
+    @property
+    def value(self) -> bool:
+        return self._value
+
+    @value.setter
+    def value(self, value: bool):
+        self._value = value
 
 
 class Clause:
 
-    def __init__(self, vars: list):
-        self.vars = vars
+    def __init__(self, literals: list):
+        self.literals = literals
+        self.var_names = []
+        self.vars = []
 
     @property
-    def vars(self) -> list:
-        return self._vars
+    def literals(self) -> list:
+        return self._literals
 
-    @vars.setter
-    def vars(self, value: list):
-        self._vars = map(lambda value: SATvar(value),value)
+    @literals.setter
+    def literals(self, value: list):
+        self._literals = list(map(lambda value: SATvar(value),value))
+        
+    def add_var(self, var_str):
+        var = SATvar(var_str)
+        if var.name not in self.var_names:
+            self.var_names.append(var.name)
+            self.vars.append(var)
 
 
 class SAT:
@@ -57,3 +64,5 @@ class NextSAT(SAT):
 
     def __init__(self, clauses: list):
         super(NextSAT, self).__init__(clauses)
+
+clause = Clause(['-1','2','-5'])

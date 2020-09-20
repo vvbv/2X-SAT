d03f1465dd9075e04bb36928db129b5c06e05263
Juan Felipe <juan.orozco.escobar@correounivalle.edu.co>
2020-09-19 19:52:26 -0500

    add import DIMACS format

--- SAT/Reductor/tools.py
+++ SAT/Reductor/tools.py
@@ -1,9 +1,23 @@
 import os
 
+def readFile(path: str):
+    if not (os.path.isfile(path)):
+        raise Exception("File does not exist in relative path: '" + path + '\'')
 
-def readFile(path):
-    f = open("")
-    pass
+    satInstance = []
+    for line in open(path, "r"):
+        if isNotClauseLine(line):
+            satInstance.append(getClauseFromLine(line))
+    return satInstance
+
+def getClauseFromLine(line: str):
+    satClause = line.replace('\n', '').replace(' 0', '').split(' ')
+    while("" in satClause):
+        satClause.remove("")
+    return satClause
+
+def isNotClauseLine(line: str):
+    return not line.startswith("c") and not line.startswith("p")
 
 def parseFile(path):
     pass

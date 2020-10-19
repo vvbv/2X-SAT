f86d3acf20f3c0f27dbf3bfd9f1e9e1c7c9f5774
vvbv <jeison.cardona@mgwebstudio.com>
2020-10-18 19:26:18 -0500

    Checkpoint

--- SAT/Reductor/main.py
+++ SAT/Reductor/main.py
@@ -1,7 +1,62 @@
+#!/usr/bin/python3.7
+
+import sys
+import getopt
 from TwoXSAT import TwoXSAT
 from tools import parseFile
 
-sat = parseFile('../InstanciasSAT/test.cnf')
 
-twoXSAT = TwoXSAT()
-print(twoXSAT.toXSAT(sat,5))
+def showHelp(odir, idir, verb):
+    print(
+        "main.py -x <X-SAT, [>= 3]> "
+        "-d <input dir [default = '" + idir + "']> "
+        "-o <output dir [default = '" + odir + "']> "
+        "-v <verbose [default = '" + 'True' if verb else 'False' + "']>")
+    sys.exit(2)
+
+
+def main(argv):
+
+    opts = None
+    xsat = None
+    verb = False
+    odir = 'X-SAT'
+    idir = '../InstanciasSAT'
+
+    try:
+        opts, args = getopt.getopt(argv, "hx:i:o:v:", ["xsat=", "idir=", 'odir=', 'verbose='])
+    except getopt.GetoptError:
+        showHelp(odir, idir, verb)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            showHelp(odir, idir, verb)
+        elif opt in ("-x", "--xsat"):
+
+            try:
+                xsat = int(arg)
+            except ValueError:
+                showHelp(odir, idir, verb)
+
+            if xsat < 3:
+                showHelp(odir, idir, verb)
+
+        elif opt in ("-d", "--idir"):
+            idir = arg
+        elif opt in ("-o", "--odir"):
+            odir = arg
+        elif opt in ("-v", "--verbose"):
+            verb = True if arg.lower() == 'true' else False
+
+    if xsat:
+
+        sat = parseFile(idir + '/test.cnf')
+
+        twoXSAT = TwoXSAT()
+        if verb:
+            print(twoXSAT.toXSAT(sat, xsat))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
--- SAT/reducir.sh
+++ SAT/reducir.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+cd Reductor
+python3.7 main.py -x $2 -v true
